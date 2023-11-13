83e67de4dde5f25936be96fefab15894014c7aad
Angela Lucia <angela@MacBook-Air-de-Angela.local>
2023-11-13 12:44:32 -0300

    Adicionei script de raspagem de dados do segundo grau

--- automacao.py
+++ automacao.py
@@ -2,6 +2,12 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.service import Service
 
+import requests
+from lxml import etree 
+
+from bs4 import BeautifulSoup
+
+
 service = Service(ChromeDriverManager().install())
 
 navegador = webdriver.Chrome(service=service)
@@ -13,4 +19,71 @@ navegador.find_element("xpath", '//*[@id="pbConsultar"]').click()
 navegador.find_element("xpath", '//*[@id="processoSelecionado"]').click()
 navegador.find_element("xpath", '//*[@id="botaoEnviarIncidente"]').click()
 
-import pdb; pdb.set_trace()
+soup = BeautifulSoup(navegador.page_source, "html.parser")
+cnj = soup.select("#numeroProcesso")[0].text.strip()
+classe = soup.select("#classeProcesso")[0].text.strip()
+area = soup.select("#areaProcesso")[0].text.strip()
+assunto = soup.select("#assuntoProcesso")[0].text.strip()
+orgao_julgador = soup.select("#orgaoJulgadorProcesso")[0].text.strip()
+valor_da_acao = soup.select("#valorAcaoProcesso")[0].text.strip()
+apelante = soup.select(
+                "#tableTodasPartes .nomeParteEAdvogado"
+            )[0].contents[0].replace("\n", "").replace("\t", "").strip()
+apelante_adv = [
+    soup.select(
+        "#tableTodasPartes .nomeParteEAdvogado"
+    )[0].contents[3].replace('\n', '').replace('\t', '').strip(),
+    soup.select(
+        "#tableTodasPartes .nomeParteEAdvogado"
+    )[0].contents[8].replace('\n', '').replace('\t', '').strip(),
+    soup.select(
+        "#tableTodasPartes .nomeParteEAdvogado"
+    )[0].contents[13].replace('\n', '').replace('\t', '').strip(),
+    soup.select(
+        "#tableTodasPartes .nomeParteEAdvogado"
+    )[0].contents[18].replace('\n', '').replace('\t', '').strip()
+]
+apelante = soup.select(
+    "#tableTodasPartes .nomeParteEAdvogado"
+)[1].contents[0].replace('\n', '').replace('\t', '').strip()
+apelante_adv = [
+    soup.select(
+        "#tableTodasPartes .nomeParteEAdvogado"
+    )[1].contents[3].replace('\n', '').replace('\t', '').strip(),
+]
+apelado = soup.select(
+    "#tableTodasPartes .nomeParteEAdvogado"
+)[2].contents[0].replace('\n', '').replace('\t', '').strip()
+apelado_adv = [
+    soup.select(
+        "#tableTodasPartes .nomeParteEAdvogado"
+    )[2].contents[3].replace('\n', '').replace('\t', '').strip(),
+]
+apelada = soup.select(
+            "#tableTodasPartes .nomeParteEAdvogado"
+        )[3].contents[0].replace('\n', '').replace('\t', '').strip(),
+apelada_adv = [
+    soup.select(
+        "#tableTodasPartes .nomeParteEAdvogado"
+    )[3].contents[3].replace('\n', '').replace('\t', '').strip()
+]
+movimentacoes = []
+trs = soup.select("#tabelaUltimasMovimentacoes tr")
+for tr in trs:
+    data_movimentacao = tr.select(".dataMovimentacaoProcesso")[0].contents[0].strip()
+    
+    conteudos =  tr.select(".descricaoMovimentacaoProcesso")[0].contents
+    if conteudos[0].text.strip():
+        titulo_movimentacao = conteudos[0].text.strip()
+    else:
+        titulo_movimentacao = conteudos[1].text.strip()
+
+    descricao_movimentacao = tr.select('span')[0].text.strip()
+
+    movimentacoes.append(
+        {
+            "data": data_movimentacao,
+            "titulo": titulo_movimentacao,
+            "movimento": descricao_movimentacao,
+        }
+    )
