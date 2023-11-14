b9560f9a428fb754d72912bd76d83e8ae3510ece
Angela Lucia <angela@MacBook-Air-de-Angela.local>
2023-11-13 23:20:38 -0300

    Adicionando condição, caso juiz e valor de aco retorne vazio

--- app/raspagem_tjce.py
+++ app/raspagem_tjce.py
@@ -1,16 +1,27 @@
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
 import requests
 from lxml import etree 
 
 from bs4 import BeautifulSoup
 
+
+
 def RasparTjce(cnj):
+    service = Service(ChromeDriverManager().install())
 
-    URL = f"https://esaj.tjce.jus.br/cpopg/show.do?processo.numero={cnj}"
-    response_raw = requests.get(URL)
-    soup = BeautifulSoup(response_raw.text, "html.parser")
+    navegador = webdriver.Chrome(service=service)
+
+
+    navegador.get("https://esaj.tjce.jus.br/cpopg/open.do")
+    navegador.find_element("xpath", '//*[@id="radioNumeroAntigo"]').click()
+    navegador.find_element("xpath", '//*[@id="nuProcessoAntigoFormatado"]').send_keys("0070337-91.2008.8.06.0001")
+    navegador.find_element("xpath", '//*[@id="botaoConsultarProcessos"]').click()
     
+    soup = BeautifulSoup(navegador.page_source, "html.parser")
     response = etree.HTML(str(soup))
-
 
     if not response.xpath('//*[@id="numeroProcesso"]/text()'):
         return None
@@ -22,17 +33,30 @@ def RasparTjce(cnj):
     data_de_distribuicao = response.xpath(
         '//*[@id="dataHoraDistribuicaoProcesso"]/text()'
     )[0].split()[0]
-    juiz = response.xpath('//*[@id="juizProcesso"]/text()')[0]
-    valor_da_acao = response.xpath(
-        '//*[@id="valorAcaoProcesso"]/text()'
-    )[0].replace(' ', '')
+    import pdb; pdb.set_trace()
+    juiz = ''
+    if response.xpath('//*[@id="juizProcesso"]/text()'):
+        juiz = response.xpath(
+            '//*[@id="juizProcesso"]/text()'
+        )[0].replace(' ', '')
+    valor_da_acao = ''
+    if response.xpath('//*[@id="valorAcaoProcesso"]/text()'):
+        valor_da_acao = response.xpath(
+            '//*[@id="valorAcaoProcesso"]/text()'
+        )[0].replace(' ', '')
+        
+
+
 
     return {
-        "cnj": cnj,
-        "classe": classe,
-        "area": area,
-        "assunto": assunto,
-        "data_de_distribuicao": data_de_distribuicao,
-        "juiz": juiz,
-        "valor_da_acao": valor_da_acao,
+        "primeiro_grau": {
+            "cnj": cnj,
+            "classe": classe,
+            "area": area,
+            "assunto": assunto,
+            "data_de_distribuicao": data_de_distribuicao,
+            "juiz": juiz,
+            "valor_da_acao": valor_da_acao,
+        }
+        
     }
